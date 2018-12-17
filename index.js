9c3dfed16335a5e86d44241f98e8c1827e3e79d7
Jorge Valle <jorge.e.valle@gmail.com>
2018-12-18 00:08:21 +0100

    Created React app.

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'),
-      app = express(),
-      port = 3000;
-
-app.get('/', (req, res) => res.sendfile('index.html'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
