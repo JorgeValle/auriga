ca46bf45666edf32e3b5d590ff825c93eb9c257a
Jorge Valle <jorge.e.valle@gmail.com>
2019-01-18 11:26:54 -0500

    Wrapped up player component, for now.

--- src/Player/PlayerMaterial.jsx
+++ src/Player/PlayerMaterial.jsx
@@ -4,6 +4,7 @@ import King from '../Pieces/King';
 import Queen from '../Pieces/Queen';
 import Bishop from '../Pieces/Bishop';
 import Rook from '../Pieces/Rook';
+import Knight from '../Pieces/Knight';
 import Pawn from '../Pieces/Pawn';
 
 // Components should represent one piece of data model
@@ -14,18 +15,29 @@ class PlayerMaterial extends React.Component {
 
     return (
       <div className="player-material">
-        <span>Pieces</span>
+        <h3>Material</h3>
         <meter min="0"  value="13" max="16"></meter>
         <span>13 / 16</span>
-  
-        <div>
-          <King />
-          <Queen />
-          <Bishop />
-          <Rook />
-          <Pawn />
+        <div className="piece-count">
+          <span>
+          1 x <King />
+          </span>
+          <span>
+          1 x <Queen />
+          </span>
+          <span>
+          2 x <Rook />
+          </span>
+          <span>
+          1 x <Bishop />
+          </span>
+          <span>
+          1 x <Knight />
+          </span>
+          <span>
+          6 x <Pawn />
+          </span>
         </div>
-
       </div>
     );
   }
